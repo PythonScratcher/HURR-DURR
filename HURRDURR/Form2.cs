b85654828cb004daf2be2a35a48ceab08bb04029
PythonScratcher <callumcardy7@gmail.com>
2022-08-29 11:56:27 +0100

    Add project files.

---
+++ HURRDURR/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HURRDURR
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+            var t = Task.Delay(1000);
+            t.Wait();
+            var form1 = new Form1();
+            form1.Show();
+        }
+    }
+}
